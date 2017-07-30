ff89f09c0408c674decf1f71ea9f0c44903b17e1
chris.evi-parker <chrisparker126@gmail.com>
2017-07-30 20:25:00 +0100

    very silly, you need to run train step several times to reduce cross  entropy

--- titanic.py
+++ titanic.py
@@ -94,7 +94,9 @@ if __name__ == '__main__':
 
     sess = tf.InteractiveSession()
     tf.global_variables_initializer().run()
-    sess.run(train_step, feed_dict={x: x_train, y_: y_train})
+
+    for i in range(1000)
+    	sess.run(train_step, feed_dict={x: x_train, y_: y_train})
 
     # Test trained model
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
